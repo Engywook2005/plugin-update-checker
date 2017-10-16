836e5abd9bfd598210e31ddce00a372285efcfb8
Greg Thorson <greg.thorson@sheknows.com>
2017-10-16 15:03:18 -0400

    Adding temporary logging ability

--- class-sparkplug-auto-update-manager.php
+++ class-sparkplug-auto-update-manager.php
@@ -10,6 +10,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
+$GLOBALS['logFunction'] = function($msg) {
+
+};
+
 require_once 'plugin-update-checker.php';
 
 class Auto_Update_Manager {
@@ -25,9 +29,13 @@ class Auto_Update_Manager {
 	private $masterPluginFILE;
 	private $masterPluginSlug;
 
+	public function __construct($enableAutoUpdates = false, $metaDataURL, $fullPath, $pluginSlug, $interval, $logFunction = null) {
+		if($logFunction) {
+			$this -> setLogFunction($logFunction);
+		}
 
+		$GLOBALS['logFunction']('hidey ho');
 
-	public function __construct($enableAutoUpdates = false, $metaDataURL, $fullPath, $pluginSlug, $interval) {
 
 		$this->plugins  = glob(WP_PLUGIN_DIR . DIRECTORY_SEPARATOR . 'familyprefix-*' . DIRECTORY_SEPARATOR .'familyprefix-*.php');
 
@@ -72,7 +80,7 @@ class Auto_Update_Manager {
 			$updater->setBranch( $this->stableBranchName );
 		}
 		*/
-
+		$GLOBALS['logFunction']("checking on auto update: $enableAutoUpdates");
 		if( $enableAutoUpdates ){
 			add_filter( 'auto_update_plugin', [$this, 'enableAutoUpdatesForPlugins'], 10, 2 );
 		}
@@ -80,12 +88,15 @@ class Auto_Update_Manager {
 	}
 
 	public function enableAutoUpdatesForPlugins($update, $item){
-
-		if( array_key_exists( $item->slug, $this->updaters ) ){
+		$GLOBALS['logFunction']("trying to do an update");
+		if( array_key_exists( $item->slug, $this->updaters )) {
 			return true;
 		}
 
 		return $update;
+	}
 
+	public function setLogFunction($logFunction) {
+		$GLOBALS['logFunction'] = $logFunction;
 	}
 }
